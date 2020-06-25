227386ccc0f411e88cf083bf068be65bf4d7f58a
cvidalmsu <cvidalmsu@gmail.com>
2020-06-25 19:02:56 +0000

    LastUpdate

--- executeBenchFD.py
+++ executeBenchFD.py
@@ -12,11 +12,26 @@ for i in cc:
         for solver in ["Sat4j"]:    
             for lmax in ["3","2","1","0"]:
                 for difficulty in ["0","50","100"]:
-                    print("    python ./fmdiag.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty)
-                    os.system("python ./fmdiag.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " resultFD.csv")
-                    if not lmax == "0":
-                        print("    python ./fmdiag_parallel_mp.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty)
-                        os.system("python ./fmdiag_parallel_mp.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " resultFD.csv")
+                    if lmax == "0":
+                        print("    python3 ./fmdiag.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty)
+                        os.system("python3 ./fmdiag.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " resultFD.csv")
+                    else:
+                        print("    python3 ./fd_parallel_mp.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty)
+                        os.system("python3 ./fd_parallel_mp.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " resultFD.csv")
 
 reqtime = time.time() - starttime
-print("Sat4j time: "+str(reqtime))
+print("GlucoseSat4j time: "+str(reqtime))
+
+for i in cc:
+    modelPath="./cnf/bench/model_"+i+".cnf"
+    for j in range(3):
+        productPath="./cnf/bench/prod-"+i+"-"+str(j)+".prod"
+        for solver in ["Choco4"]:    
+            for lmax in ["3","2","1","0"]:
+                for difficulty in ["0","50","100"]:
+                    os.system("python ./fmdiag.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " resultFD_choco4.csv")
+                    if not lmax == "0":
+                        os.system("python ./fd_parallel_mp.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " resultFD_choco4.csv")
+
+reqtime = time.time() - starttime
+print("Choco4 time: "+(reqtime))
