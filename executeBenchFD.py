6e2bce6149d2372e334cd4b11704ccdbdf04ff9d
cvidalmsu <cvidalmsu@gmail.com>
2020-06-25 19:10:25 +0000

    LastVersion

--- executeBenchFD.py
+++ executeBenchFD.py
@@ -20,18 +20,4 @@ for i in cc:
                         os.system("python3 ./fd_parallel_mp.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " resultFD.csv")
 
 reqtime = time.time() - starttime
-print("GlucoseSat4j time: "+str(reqtime))
-
-for i in cc:
-    modelPath="./cnf/bench/model_"+i+".cnf"
-    for j in range(3):
-        productPath="./cnf/bench/prod-"+i+"-"+str(j)+".prod"
-        for solver in ["Choco4"]:    
-            for lmax in ["3","2","1","0"]:
-                for difficulty in ["0","50","100"]:
-                    os.system("python ./fmdiag.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " resultFD_choco4.csv")
-                    if not lmax == "0":
-                        os.system("python ./fd_parallel_mp.py " + modelPath + " "+productPath+" "+lmax+" "+solver+" "+difficulty+" >>"+ " resultFD_choco4.csv")
-
-reqtime = time.time() - starttime
-print("Choco4 time: "+(reqtime))
+print("Sat4j time: "+str(reqtime))
